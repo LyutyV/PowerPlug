470ac40f8802cda335b977893502e963f5609af7
diklab <diklab@DIKLA-WIN10>
2016-04-05 12:02:13 +0300

    fg

--- Powerplug.Web/app/controllers/powerplan/savingPlanEditorCtrl.js
+++ Powerplug.Web/app/controllers/powerplan/savingPlanEditorCtrl.js
@@ -10,7 +10,7 @@
                 link: function(scope, element, attrs, ngModel) {
                     ngModel.$parsers.push(function(val) {
                         return val ? parseInt(val, 10) : null;
-                    });
+                    }); 
                     ngModel.$formatters.push(function(val) {
                         return val ? '' + val : null;
                     });
