632ff2839f5c56893a4939155ec214e0fe4394c2
ronib_pp <ronib@powerplugltd.com>
2016-04-11 11:41:58 +0300

    add comuoter to pp

---
+++ Powerplug.Web/app/controllers/powerplan/partials/savingPlanEditorComputers.js
@@ -0,0 +1,18 @@
+﻿var computersHandler = {
+    vm: {},
+    init: function (vm) {
+        computersHandler.vm = vm;
+    },
+    addComputers: function () {
+        if(computersHandler.vm.addedComputers){
+            computersHandler.vm.computers = [];
+        }        
+        
+        for (var i = 0; i < computersHandler.vm.computers.length; i++) {
+            if (targetArray.indexOf(checkerArray[i]) === -1) {
+                //addedComputers.push[]
+            }
+        }
+        
+    }
+};
